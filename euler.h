5ed4f6ea1ef80542f7d07edd1d1f9b27d64ee7c5
fau20 <90728996+fau20@users.noreply.github.com>
2021-09-26 16:45:05 -0400

    Add files via upload

---
+++ euler.h
@@ -0,0 +1,2 @@
+int multiples();
+int sumSquare();
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "euler.h"
+
+int main() {
+  printf("The sum of all multiples of 3 or 5 below 1000 is %d", multiples());
+  printf("\nThe difference between the sum of the squares of the first one hundred natural numbers and the square of the sum is %d", sumSquare());
+  return 0;
+}
